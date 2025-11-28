5c006b62d4af664df95c2f78d7cbbbd3a40003bf
Your Name <you@example.com>
2025-11-27 23:49:09 -0500

    section 3 v2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,44 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # Có thể thêm các field khác nếu muốn
+
+    def __str__(self):
+        return self.name  # Hiển thị tên khi in object
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Quan hệ Many-to-One
+    dealer_id = models.IntegerField()  # Id đại lý, liên kết Cloudant
+    name = models.CharField(max_length=100)
+
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # Thêm loại khác nếu muốn
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
+
+    # Có thể thêm các field khác nếu muốn
+
+    def __str__(self):
+        return f"{self.car_make.name} - {self.name}"  # Hiển thị CarMake + CarModel
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
